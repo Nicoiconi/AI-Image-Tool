a9226fa206ab2510964da6e298b77a2a59cc637c
Nicolas <nicolas.iconicoff@gmail.com>
2024-02-18 22:56:08 -0300

    shared components

---
+++ components/shared/Header/Header.tsx
@@ -0,0 +1,21 @@
+
+interface Props {
+  title: string
+  subtitle?: string
+}
+
+export default function Header({ title, subtitle }: Props) {
+  return (
+    <>
+      <h2 className="h2-bold text-dark-600">
+        {title}
+      </h2>
+      {
+        subtitle &&
+        <p className="p-16-regular mt-4">
+          {subtitle}
+        </p>
+      }
+    </>
+  )
+}
